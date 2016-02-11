e1e7d3136be16f42c712c0a35433e560b427fa37
Matthew Lundquist <mlundqu1@binghamton.edu>
2016-02-11 16:15:20 -0500

    Adding calculation to WSR

--- Portfolios/example-portfolio/2_wilcox-rank/wilcox-test.r
+++ Portfolios/example-portfolio/2_wilcox-rank/wilcox-test.r
@@ -15,10 +15,20 @@ setosa <- iris$Sepal.Length[iris$Species == "setosa"]
 versicolor <- iris$Sepal.Length[iris$Species == "versicolor"]
 
 #Calculate manually TODO
-
+#This does not work yet!!! -----------------------------
 n <- length(setosa)
+d <- setosa - versicolor
+dneg <- d[d>0]
+count_neg <- as.data.frame(table(dneg))
+count_neg <- as.vector(count_neg['Freq'])
+dpos <- d[d>0]
+count_pos <- as.data.frame(table(dpos))
+count_pos <- as.vector(count_pos['Freq'])
 
+R <- dpos
+count_ <- count_neg + count_pos
 
+#-------------------------------------------------------
 
 #Run the test using the build in function
 wilcox.test(setosa,versicolor,paired=TRUE)
